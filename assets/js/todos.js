f3d94c17389f365bbae22a86ee66aac1e6409429
Jon Olson <jonolson64@outlook.com>
2017-06-17 20:57:56 -0400

    complete project

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,24 @@
 // Check off specific todos by clicking
-$('li').click(function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
-// Click on X to delete todo
-$('span').click(function(e) {
-  $(this).parent().fadeOut(1000, function() {
+// Click on trash icon to delete todo
+$("ul").on("click", "span", function(e) {
+  $(this).parent().fadeOut(400, function() {
     $(this).remove();
   });
   e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e) {
+  if(e.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append('<li><span><i class="fa fa-trash"></i></span> ' + todoText + "</li>");
+  }
+});
+
+$(".fa-plus").click(function() {
+  $("input[type='text']").fadeToggle();
 });
